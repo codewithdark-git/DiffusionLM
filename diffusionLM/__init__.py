d82c7ba8f7dffef303b06270a80a45435e2c2822
Dark Coder <codewithdark90@gmail.com>
2025-04-05 15:23:44 +0500

    fix the modules in INIT file.

--- diffusionLM/__init__.py
+++ diffusionLM/__init__.py
@@ -1,12 +1,23 @@
 """DiffusionLM: A Diffusion-based Language Model Package"""
 
-from utils.error_handler import setup_logging, DiffusionLMError, handle_errors
-from trainer.trainer import trainer, TrainingError
-from trainer.evaluate import evaluate
-from model_save.model_save import save_model, load_model, ModelSaveError
-from model_save.register_model import registerANDpush, ModelRegistrationError
-from utils.dataset import PYTORCH_Dataset, DatasetError
-from utils.datasetANDtokenizer import prepare_dataset, DatasetPreparationError
+from .trainer import trainer, TrainingError, evaluate
+from .model import DiffusionLLM, DiffusionConfig
+from .save_model import (
+        save_model, 
+        load_model, 
+        ModelSaveError, 
+        registerANDpush,
+        ModelRegistrationError,
+        )
+from .utils import (
+        prepare_dataset, 
+        DatasetPreparationError, 
+        PYTORCH_Dataset, 
+        DatasetError,
+        setup_logging,
+        DiffusionLMError,
+        handle_errors,
+        )
 
 __version__ = "0.1.0"
 
@@ -15,6 +26,8 @@ setup_logging()
 
 
 __all__ = [
+    "DiffusionLLM",
+    "DiffusionConfig",
     "setup_logging",
     "DiffusionLMError",
     "handle_errors",
